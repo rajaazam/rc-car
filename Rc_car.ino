b7541676a6a888c9b26bad36fa2b9d5607d163a4
rajaazam <50084470+rajaazam@users.noreply.github.com>
2021-02-15 17:24:44 +0500

    Add files via upload
    crc car

---
+++ Rc_car.ino
@@ -0,0 +1,89 @@
+#include <AFMotor.h>
+
+AF_DCMotor motor1(1, MOTOR12_64KHZ);
+AF_DCMotor motor2(2, MOTOR12_64KHZ);
+AF_DCMotor motor3(3, MOTOR34_64KHZ);
+AF_DCMotor motor4(4, MOTOR34_64KHZ);
+
+void setup() {
+  pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(9, OUTPUT);
+  Serial.begin(9600);
+}
+
+char t;
+
+void loop() {
+  if(Serial.available() > 0){
+    t = Serial.read();
+    stop();
+    Serial.println(t);
+
+switch(t){
+  case 'F':
+  forward();
+  break;
+  case 'B':
+  break;
+  case 'L':
+  break;
+  case 'R':
+  break;
+}
+}
+}
+
+void forward(){
+  motor1.setSpeed(255);
+  motor1.run(FORWARD);
+  motor2.setSpeed(255);
+  motor2.run(FORWARD);
+  motor3.setSpeed(255);
+  motor3.run(FORWARD);
+  motor4.setSpeed(255);
+  motor4.run(FORWARD);
+}
+void backward(){
+  motor1.setSpeed(255);
+  motor1.run(BACKWARD);
+  motor2.setSpeed(255);
+  motor2.run(BACKWARD);
+  motor3.setSpeed(255);
+  motor3.run(BACKWARD);
+  motor4.setSpeed(255);
+  motor4.run(BACKWARD);
+}
+void left(){
+  motor1.setSpeed(255);
+  motor1.run(FORWARD);
+  motor2.setSpeed(255);
+  motor2.run(FORWARD);
+  motor3.setSpeed(255);
+  motor3.run(FORWARD);
+  motor4.setSpeed(255);
+  motor4.run(FORWARD);
+}
+void right(){
+  motor1.setSpeed(255);
+  motor1.run(FORWARD);
+  motor2.setSpeed(255);
+  motor2.run(FORWARD);
+  motor3.setSpeed(255);
+  motor3.run(FORWARD);
+  motor4.setSpeed(255);
+  motor4.run(FORWARD);
+}
+void stop(){
+  motor1.setSpeed(255);
+  motor1.run(RELEASE);
+  motor2.setSpeed(255);
+  motor2.run(RELEASE);
+  motor3.setSpeed(255);
+  motor3.run(RELEASE);
+  motor4.setSpeed(255);
+  motor4.run(RELEASE);
+}
+© 2021 GitHub, Inc.
